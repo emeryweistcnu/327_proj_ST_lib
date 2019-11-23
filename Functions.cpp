6804a554a063545600d672fc9fd3c2855734a099
Emery Weist <emery.weist.17@cnu.edu>
2019-11-22 23:16:48 -0500

    comment cleanup

--- Functions.cpp
+++ Functions.cpp
@@ -15,9 +15,6 @@
 #include "./includes/Watch.h"
 #include "./includes/constants.h"
 
-//create a vector with appropriate numbers of Smalltalk_Brit,Smalltalk_American and ST_American_DonutEnthusiest
-//objects using unique pointers.  Since we are using c++11 returning this vector by value is fine since the 
-//compiler will move the vector on return rather than recreate it (this means there is no copy penalty)
 std::vector<std::unique_ptr<Smalltalk>> getPeople(int numBrit,
 		int numAmerican, int numbAmericanDonutEnthusiest,
 		int numWatches)
@@ -42,10 +39,6 @@ std::vector<std::unique_ptr<Smalltalk>> getPeople(int numBrit,
 		people.push_back(std::move(d));
 	}
 
-	//create some watches (as long as number watches <= numb people)
-	//then give the watches away to first NUM_WATCHES people in the vector
-	// when you are finished using the vector you return
-	//from this function(see Smalltalk header for hints)
 	for (int i = 0 ; i < numWatches && i < people.size(); i++)
 	{
 		std::unique_ptr<Watch> w(new Watch());
